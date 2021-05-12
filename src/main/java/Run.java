b91bdd59e16e7dd937f2f3daceff1d400c979d2a
unknown <igor.lirussi@studio.unibo.it>
2021-05-12 10:52:00 +0200

    added tests for sourcecode

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -3,4 +3,8 @@ public class Run {
     public static void main(String[] args) {
         System.out.println("Programma Eseguito!");
     }
+    
+    public static int sum(int x, int y) {
+    	return y+x;
+    }
 }
